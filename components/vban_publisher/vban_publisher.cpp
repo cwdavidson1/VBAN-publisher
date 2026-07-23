7c89e2842ff9b53379703bd6a9b8b61f4147214d
cwdavidson1 <36550971+cwdavidson1@users.noreply.github.com>
2026-07-23 13:08:59 -0400

    Update vban_publisher.cpp

--- components/vban_publisher/vban_publisher.cpp
+++ components/vban_publisher/vban_publisher.cpp
@@ -59,18 +59,7 @@ void VBANPublisherComponent::loop() {
       // Deallocate buffers, if necessary
       this->stop_();
 
-      // Reset sensor outputs
-      if (this->peak_sensor_ != nullptr) {
-        this->peak_sensor_->publish_state(NAN);
-      }
-      if (this->rms_sensor_ != nullptr) {
-        this->rms_sensor_->publish_state(NAN);
-      }
-
-      // Reset accumulators
-      this->squared_peak_ = 0;
-      this->squared_samples_sum_ = 0;
-      this->sample_count_ = 0;
+      
     }
 
     return;
@@ -93,55 +82,37 @@ void VBANPublisherComponent::loop() {
   ESP_LOGD(TAG,
                 "VBAN Publisher available: %d ",this->audio_source_->available());
 
-  const uint32_t samples_in_window =
-      this->microphone_source_->get_audio_stream_info().ms_to_samples(this->measurement_duration_ms_);
-  const uint32_t samples_available_to_process =
-      this->microphone_source_->get_audio_stream_info().bytes_to_samples(this->audio_source_->available());
-  const uint32_t samples_to_process = std::min(samples_in_window - this->sample_count_, samples_available_to_process);
+  // const uint32_t samples_in_window =
+  //     this->microphone_source_->get_audio_stream_info().ms_to_samples(this->measurement_duration_ms_);
+  // const uint32_t samples_available_to_process =
+  //     this->microphone_source_->get_audio_stream_info().bytes_to_samples(this->audio_source_->available());
+  // const uint32_t samples_to_process = std::min(samples_in_window - this->sample_count_, samples_available_to_process);
 
   // MicrophoneSource always provides int16 samples due to Python codegen settings
-  const int16_t *audio_data = reinterpret_cast<const int16_t *>(this->audio_source_->data());
+ // const int16_t *audio_data = reinterpret_cast<const int16_t *>(this->audio_source_->data());
 
   // Process all the new audio samples
-  for (uint32_t i = 0; i < samples_to_process; ++i) {
-    // Squaring int16 samples won't overflow an int32
-    int32_t squared_sample = static_cast<int32_t>(audio_data[i]) * static_cast<int32_t>(audio_data[i]);
+  // for (uint32_t i = 0; i < samples_to_process; ++i) {
+  //   // Squaring int16 samples won't overflow an int32
+  //   int32_t squared_sample = static_cast<int32_t>(audio_data[i]) * static_cast<int32_t>(audio_data[i]);
 
-    if (this->peak_sensor_ != nullptr) {
-      this->squared_peak_ = std::max(this->squared_peak_, squared_sample);
-    }
+  //   if (this->peak_sensor_ != nullptr) {
+  //     this->squared_peak_ = std::max(this->squared_peak_, squared_sample);
+  //   }
 
-    if (this->rms_sensor_ != nullptr) {
-      // Squared sum is an uint64 type - at max levels, an uint32 type would overflow after ~8 samples
-      this->squared_samples_sum_ += squared_sample;
-    }
+  //   if (this->rms_sensor_ != nullptr) {
+  //     // Squared sum is an uint64 type - at max levels, an uint32 type would overflow after ~8 samples
+  //     this->squared_samples_sum_ += squared_sample;
+  //   }
 
-    ++this->sample_count_;
-  }
+  // ++this->sample_count_;
+  //}
 
   // Remove the processed samples from ``audio_source_``
-  this->audio_source_->consume(this->microphone_source_->get_audio_stream_info().samples_to_bytes(samples_to_process));
-
-  if (this->sample_count_ == samples_in_window) {
-    // Processed enough samples for the measurement window, compute and publish the sensor values
-    if (this->peak_sensor_ != nullptr) {
-      const float peak_db = 10.0f * log10f(static_cast<float>(this->squared_peak_) / MAX_SAMPLE_SQUARED_DENOMINATOR);
-      this->peak_sensor_->publish_state(peak_db);
-
-      this->squared_peak_ = 0;  // reset accumulator
-    }
-
-    if (this->rms_sensor_ != nullptr) {
-      // Calculations are done with doubles instead of floats - floats lose precision for even modest window durations
-      const double rms_db = 10.0 * log10((this->squared_samples_sum_ / MAX_SAMPLE_SQUARED_DENOMINATOR) /
-                                         static_cast<double>(samples_in_window));
-      this->rms_sensor_->publish_state(rms_db);
-
-      this->squared_samples_sum_ = 0;  // reset accumulator
-    }
-
-    this->sample_count_ = 0;  // reset counter
-  }
+  //this->audio_source_->consume(this->microphone_source_->get_audio_stream_info().samples_to_bytes(samples_to_process));
+  this->audio_source_->consume(this->audio_source_->available());
+  
+  
 }
 
 void VBANPublisherComponent::start() {
